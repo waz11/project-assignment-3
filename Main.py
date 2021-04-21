218f00b5b4925fa13aa01088a3ee76a50c79a8eb
Ron <ronisr@post.bgu.ac.il>
2021-04-21 12:41:19 +0300

    First Commit

--- Main.py
+++ Main.py
@@ -1,1 +1,21 @@
-print("Good luck Teamates!")
+import sqlite3
+
+
+# data_path = "./files/database.sqlite"
+
+def main():
+    # Create a SQL connection to our SQLite database
+    con = sqlite3.connect("./files/database.sqlite")
+
+    cur = con.cursor()
+
+    # The result of a "cursor.execute" can be iterated over by row
+    for row in cur.execute('SELECT * from Country'):
+        print(row)
+
+    # Be sure to close the connection
+    con.close()
+
+if __name__ == "__main__":
+    main()
+
