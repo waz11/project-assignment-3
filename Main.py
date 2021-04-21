d243448467d346d3dd890d333802d46532809f2d
Ron <ronisr@post.bgu.ac.il>
2021-04-21 12:52:04 +0300

    First Commit

--- Main.py
+++ Main.py
@@ -1,20 +1,16 @@
 import sqlite3
+import pandas as pd
 
+data_path = "./files/database.sqlite"
 
-# data_path = "./files/database.sqlite"
+def read_data():
+    # Create a SQL connection to our SQLite database
+    con = sqlite3.connect(data_path)
+    return pd.read_sql_query("SELECT * FROM Country", con)
 
 def main():
-    # Create a SQL connection to our SQLite database
-    con = sqlite3.connect("./files/database.sqlite")
-
-    cur = con.cursor()
-
-    # The result of a "cursor.execute" can be iterated over by row
-    for row in cur.execute('SELECT * from Country'):
-        print(row)
-
-    # Be sure to close the connection
-    con.close()
+    ds = read_data()
+    print(ds)
 
 if __name__ == "__main__":
     main()
