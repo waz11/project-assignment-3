89b48efb793629d2bf2f262f5cee2dbb04374cb3
Ron <ronisr@post.bgu.ac.il>
2021-04-21 13:24:59 +0300

    Read Data

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import ReadData
 
 def main():
     country = ReadData.get_table("SELECT * FROM Country")
-    match = ReadData.get_table("SELECT * FROM Match")
+    match = ReadData.get_table("select * from Match where season != '2015/2016'")
     player = ReadData.get_table("SELECT * FROM Player")
     player_attributes = ReadData.get_table("SELECT * FROM Player_Attributes")
     team = ReadData.get_table("SELECT * FROM Team")
