aed8fad7d372162eb7a5ed0a2d7d39d86e7b1330
Ron <ronisr@post.bgu.ac.il>
2021-04-21 13:02:50 +0300

    Read Data

--- Main.py
+++ Main.py
@@ -1,16 +1,31 @@
 import sqlite3
 import pandas as pd
+import numpy as np
 
 data_path = "./files/database.sqlite"
+
+def fillMissingValues(df):
+    for col in df.columns:
+        if np.issubdtype(df[col].dtype, np.number):
+            df[col].fillna(df[col].mean(), inplace=True)
+    return df;
 
 def read_data(query):
     # Create a SQL connection to our SQLite database
     con = sqlite3.connect(data_path)
-    return pd.read_sql_query(query, con)
+    data = pd.read_sql_query(query, con)
+    return fillMissingValues(data)
 
 def main():
-    ds_contries = read_data("SELECT * FROM Country")
-    print(ds_contries)
+    country = read_data("SELECT * FROM Country")
+    match = read_data("SELECT * FROM Match")
+    player = read_data("SELECT * FROM Player")
+    player_attributes = read_data("SELECT * FROM Player_Attributes")
+    team = read_data("SELECT * FROM Team")
+    team_atributes = read_data("SELECT * FROM Team_Attributes")
+
+
+
 
 if __name__ == "__main__":
     main()
