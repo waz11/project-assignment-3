f2aa829c5e4ddf7d236cd7b97e748a33f5add594
Ron <ronisr@post.bgu.ac.il>
2021-04-22 19:01:01 +0300

    Read Data

--- Main.py
+++ Main.py
@@ -1,22 +1,21 @@
 import ReadData
 
 def main():
-    # country = ReadData.get_table("SELECT * FROM Country")
-    # match = ReadData.get_table("select * from Match where season != '2015/2016'")
-    # player = ReadData.get_table("SELECT * FROM Player")
-    # player_attributes = ReadData.get_table("SELECT * FROM Player_Attributes")
-    # team = ReadData.get_table("SELECT * FROM Team")
-    # team_attributes = ReadData.get_table("SELECT * FROM Team_Attributes")
-    dataTable = ReadData.get_table("SELECT Country.name AS Country, League.name AS League_Name, HomeTeam.team_long_name AS Home_Team ,AwayTeam.team_long_name AS Away_Team, Match.season, Match.stage, Match.home_team_goal, Match.away_team_goal \
-FROM Match \
-JOIN League,Country \
-ON Country.id = Match.country_id and League.id = Match.league_id \
-LEFT JOIN Team AS HomeTeam ON HomeTeam.team_api_id = Match.home_team_api_id \
-LEFT JOIN Team as AwayTeam ON AwayTeam.team_api_id = Match.away_team_api_id \
-WHERE Match.season <> '2015/2016'")
-    print(dataTable)
+    match, player, player_attributes, team, team_attributes, country = ReadData.getTables()
+    print(match)
 
 
 if __name__ == "__main__":
     main()
 
+
+
+# lev's query
+#     dataTable = ReadData.get_table("SELECT Country.name AS Country, League.name AS League_Name, HomeTeam.team_long_name AS Home_Team ,AwayTeam.team_long_name AS Away_Team, Match.season, Match.stage, Match.home_team_goal, Match.away_team_goal \
+#     FROM Match \
+#     JOIN League,Country \
+#     ON Country.id = Match.country_id and League.id = Match.league_id \
+#     LEFT JOIN Team AS HomeTeam ON HomeTeam.team_api_id = Match.home_team_api_id \
+#     LEFT JOIN Team as AwayTeam ON AwayTeam.team_api_id = Match.away_team_api_id \
+#     WHERE Match.season <> '2015/2016'")
+#     print(dataTable)
--- ReadData.py
+++ ReadData.py
@@ -10,11 +10,21 @@ def fillMissingValues(df):
             df[col].fillna(df[col].mean(), inplace=True)
     return df;
 
-def get_table(query):
+def __get_table(query):
     # Create a SQL connection to our SQLite database
     con = sqlite3.connect(data_path)
-    data = pd.read_sql_query(query, con)
+    df = pd.read_sql_query(query, con)
     con.close()
-    return fillMissingValues(data)
+    return df
+
+def getTables():
+    match = __get_table("select * from Match where season != '2015/2016'")
+    player = __get_table("SELECT * FROM Player")
+    player_attributes = __get_table("SELECT * FROM Player_Attributes")
+    team = __get_table("SELECT * FROM Team")
+    team_attributes = __get_table("SELECT * FROM Team_Attributes")
+    country = __get_table("SELECT * FROM Country")
+
+    return match, player, player_attributes, team, team_attributes, country
 
 
