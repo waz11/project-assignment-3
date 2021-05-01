72446a847fbb11ccc4c3465b45e0fde0a0cdd77e
noyaka <noyakaravani4@gmail.com>
2021-05-01 19:11:01 +0300

    extract to csv

--- Data.py
+++ Data.py
@@ -7,11 +7,11 @@ home_players = ['home_player_' + str(x + 1) for x in range(11)]
 away_players = ['away_player_' + str(x + 1) for x in range(11)]
 problematic_attributes = home_players + away_players
 def fillMissingValues(df):
-    # for col in df.columns:
-    #     if np.issubdtype(df[col].dtype, np.number) and col in problematic_attributes:
-    #         df[col].fillna(0, inplace=True)
-    #     if np.issubdtype(df[col].dtype, np.number):
-    #         df[col].fillna(df[col].mean(), inplace=True)
+    for col in df.columns:
+        if np.issubdtype(df[col].dtype, np.number) and col in problematic_attributes:
+            df[col].fillna(0, inplace=True)
+        if np.issubdtype(df[col].dtype, np.number):
+            df[col].fillna(df[col].mean(), inplace=True)
     return df
 
 
